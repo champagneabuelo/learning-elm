b38ed78df27925490934e2a520cb72e530443a40
Jason <jev4zs@virginia.edu>
2018-02-25 20:08:08 -0500

    completed elmbridge tutorial

---
+++ elmoji-translator-release-0/Model.elm
@@ -0,0 +1,25 @@
+module Model exposing (..)
+
+type Direction 
+    = TextToEmoji
+    | EmojiToText
+
+
+type alias Model =
+    { currentText : String 
+    , direction : Direction
+    , selectedKey : String
+    }
+
+
+defaultKey : String
+defaultKey =
+    "😄"
+
+
+init : Model
+init =
+    { currentText = "" 
+    , direction = TextToEmoji
+    , selectedKey = defaultKey
+    }
